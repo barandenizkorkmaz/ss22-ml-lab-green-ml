f76bab2bd12d9a3ee64e52de039039c5a3beee5e
barandenizkorkmaz <deniz.korkmaz@boun.edu.tr>
2022-06-05 17:24:27 +0200

    Add abstract data classes

---
+++ src/dataset/data/base_dataloader.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+
+
+class Dataloader(ABC):
+    '''
+    tbd
+    '''
+    def __init__(self):
+        pass
---
+++ src/dataset/data/base_dataset.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Dataset(ABC):
+    def __int__(self, root_path):
+        self.root_path = root_path
+        self.x = None # np.ndarray
+        self.y = None # np.ndarray
+
+    @abstractmethod
+    def load(self):
+        '''
+            Initializes self.x and self.y numpy arrays which will contain the dataset given in the root_path.
+        '''
