3b6b11f5f2bcb5722823bf28abe8be7792090b62
barandenizkorkmaz <deniz.korkmaz@boun.edu.tr>
2022-07-08 01:28:19 +0200

    Add LayerwiseDatasetv2

--- src/dataset/data/dataset.py
+++ src/dataset/data/dataset.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 import ast
 from .utils import indexify, one_hotify, flatten, convert_shapes, extract_layer_features, pad_to_dense, get_layer_type
 
-
 class LayerWiseDataset(Dataset):
     def __init__(self, **kwargs):
         super().__init__()
@@ -44,7 +43,6 @@ class LayerWiseDataset(Dataset):
         return x, y
 
     def prepare(self, **kwargs):
-        is_model_wise = kwargs['is_model_wise']
         target_layer = kwargs['target_layer']
         layer_set = {'GlobalAveragePooling2D', 'ZeroPadding2D', 'BatchNormalization', 'AveragePooling2D', 'Dropout',
                      'DepthwiseConv2D', 'Multiply', 'ReLU', 'Flatten', 'MaxPooling2D', 'Add', 'Conv2D', 'Normalization',
@@ -53,23 +51,19 @@ class LayerWiseDataset(Dataset):
         conv_features = ["input_shape","output_shape","filters", "kernel_size", "stride", "num_flops"]
         pool_features = ["input_shape","output_shape","filters (default=1)", "pool_size", "stride", "num_flops"]
         # TODO: The power of kernel/pool size can be taken wrt the dim (2d/3d).
-        if is_model_wise:
-            x, y = self.extract_model_dataset(target_layer)
-            return x, y
-        else:
-            x = []
-            y = []
-            for model_index, row in self.data.iterrows():
-                model = tf.keras.models.model_from_json(row['result.model'])
-                power_layerwise = ast.literal_eval(row["result.power_layerwise"])
-                for layer, power in zip(model.layers, power_layerwise):
-                    if target_layer in layer.__class__.__name__.lower():
-                        layer_features = extract_layer_features(layer)
-                        if layer_features != False:
-                            x.append(layer_features)
-                            y.append(power)
-            x = pad_to_dense(x)
-            return np.array(x, dtype=np.uint16), np.array(y, dtype=float)
+        x = []
+        y = []
+        for model_index, row in self.data.iterrows():
+            model = tf.keras.models.model_from_json(row['result.model'])
+            power_layerwise = ast.literal_eval(row["result.power_layerwise"])
+            for layer, power in zip(model.layers, power_layerwise):
+                if target_layer in layer.__class__.__name__.lower():
+                    layer_features = extract_layer_features(layer)
+                    if layer_features != False:
+                        x.append(layer_features)
+                        y.append(power)
+        x = pad_to_dense(x)
+        return np.array(x, dtype=np.uint16), np.array(y, dtype=float)
 
     def preprocessing(self, x, y):
         if type(y) is list:
@@ -87,6 +81,85 @@ class LayerWiseDataset(Dataset):
         if validation_split != False:
             self.x_train, self.x_validation, self.y_train, self.y_validation = split(self.x_train, self.y_train, split_ratio=validation_split, shuffle=False, seed=None)
 
+class LayerWiseDatasetv2(Dataset):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.file_path = kwargs['file_path']
+        self.subset = kwargs['subset']
+        self.load()
+        self.raw_x, self.raw_y, self.y_modelwise = self.get_raw_dataset(**kwargs)
+        self.get_raw_splits(**kwargs)
+        self.x_train, self.y_train = self.raw_split_to_training_split(self.raw_x_train, self.raw_y_train,**kwargs)
+        self.x_test, self.y_test = self.raw_split_to_training_split(self.raw_x_test, self.raw_y_test,**kwargs)
+        if kwargs['validation_split']:
+            self.x_validation, self.y_validation = self.raw_split_to_training_split(self.raw_x_val, self.raw_y_val,**kwargs)
+
+    def load(self):
+        raw_data = pd.read_csv(self.file_path)
+        if self.subset == 'all':  # do nothing
+            self.data = raw_data
+        else:  # Options for subset are ['pretrained', 'simple'].
+            self.data = raw_data.loc[raw_data['result.type'] == self.subset]
+
+    def get_raw_dataset(self, **kwargs):
+        x = []
+        y_layerwise = []
+        y_modelwise = []
+        for model_index, row in self.data.iterrows():
+            current_model = []
+            current_y_layerwise = []
+            model = tf.keras.models.model_from_json(row['result.model'])
+            power_model = row["result.power"]
+            power_layerwise = ast.literal_eval(row["result.power_layerwise"])
+            for layer, power_layer in zip(model.layers, power_layerwise):
+                layer_features = extract_layer_features(layer)
+                if layer_features != False:
+                    layer_type = get_layer_type(layer)
+                    layer_features.insert(0, layer_type)
+                    current_model.append(layer_features)
+                    current_y_layerwise.append(power_layer)
+
+            if len(current_model) > 0:  # If any match for the corresponding layer is found on the current model
+                x.append(current_model)
+                y_modelwise.append(power_model)
+                y_layerwise.append(current_y_layerwise)
+        print(f"(getRawDataset)\tNumber of Models in the Dataset: {len(x)}")
+        return x, y_layerwise, y_modelwise
+
+    def get_raw_splits(self, **kwargs):
+        x, y, y_modelwise = shuffle_iterables(True, 123, self.raw_x, self.raw_y, self.y_modelwise)
+        self.raw_x_train, self.raw_x_test = split_2(x, split_ratio=kwargs['test_split'])
+        self.raw_y_train, self.raw_y_test = split_2(y, split_ratio=kwargs['test_split'])
+        self.y_modelwise_train, self.y_modelwise_test = split_2(y_modelwise, split_ratio=kwargs['test_split'])
+        if kwargs['validation_split']:
+            self.raw_x_train, self.raw_x_val = split_2(self.raw_x_train, split_ratio=kwargs['validation_split'])
+            self.raw_y_train, self.raw_y_val = split_2(self.raw_y_train, split_ratio=kwargs['validation_split'])
+            self.y_modelwise_train, self.y_modelwise_val = split_2(self.y_modelwise_train, split_ratio=kwargs['validation_split'])
+        else:
+            self.raw_x_val, self.raw_y_val, self.y_modelwise_val = None, None, None
+
+    def raw_split_to_training_split(self, raw_x_split, raw_y_split, **kwargs):
+        target_layer = kwargs['target_layer']
+        tmp_x = []
+        tmp_y = []
+        for model_x, model_y in zip(raw_x_split, raw_y_split):
+            for layer_x, layer_y in zip(model_x, model_y):
+                if layer_x[0] == target_layer:
+                    tmp_x.append(layer_x[1:])
+                    tmp_y.append(layer_y)
+        tmp_x = np.array(tmp_x, dtype=np.uint16)
+        tmp_y = np.array(tmp_y, dtype=np.float)
+        return tmp_x, tmp_y
+
+
+    def preprocessing(self, x, y):
+        if type(y) is list:
+            y = [tmp*1e9 for tmp in y]
+        else:
+            y = y * 1e9
+        #y = np.abs(y)  # Take abs due to issues with CodeCarbon
+        #y = (y - np.min(y)) / (np.max(y) - np.min(y))  # Normalize
+        return x, y
 
 def split(x, y, split_ratio, shuffle, seed):
     '''
@@ -106,6 +179,52 @@ def split(x, y, split_ratio, shuffle, seed):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=split_ratio, random_state=seed, shuffle=shuffle)
     return x_train, x_test, y_train, y_test
 
+
+def split_2(x, split_ratio):
+    """
+    Function for manually splitting a list by a given split_ratio
+    Args:
+        x: list
+        split_ratio: float
+
+    Returns:
+        split_1, split_2: list, list
+
+    """
+    split_point = int(len(x) * (1 - split_ratio))
+    return x[:split_point], x[split_point:]
+
+def shuffle_iterables(shuffle, seed, *args):
+    """
+    Shuffles any number of lists with each indices connected (corresponding to each other).
+
+    Args:
+        shuffle: bool
+        seed: int
+        *args: any number of lists that you want to shuffle simultaneously
+
+    Returns:
+        tuple of lists in the same order
+
+    """
+    it = iter(args)
+    the_len = len(next(it))
+    if not all(len(l) == the_len for l in it):
+        raise ValueError('not all lists have same length!')
+    if not shuffle:
+        return args
+    else:
+        iters = list()
+        for _ in range(len(args)):
+            iters.append(list())
+        import random
+        random.seed(seed)
+        shuffled_order = list(range(len(args[0])))
+        random.shuffle(shuffled_order)
+        for index in shuffled_order:
+            for i, iterable in enumerate(iters):
+                iters[i].append(args[i][index])
+        return tuple(iters)
 
 class ModelWiseDataset(Dataset):
     def __init__(self, **kwargs):
