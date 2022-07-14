90570f5020846d8e0ad37b527d5011b2f2b515ac
barandenizkorkmaz <deniz.korkmaz@boun.edu.tr>
2022-07-14 19:37:03 +0200

    Fix: Evaluation layer-wise models pipeline

--- src/evaluation/evaluate_layerwise_models.py
+++ src/evaluation/evaluate_layerwise_models.py
@@ -8,41 +8,92 @@ from src.models.metrics import *
 yaml_path = '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/run_config_layerwise.yaml'
 config = yaml.safe_load(Path(yaml_path).read_text())
 
-method_name = "CatBoost_BAG_L1" # Please enter the name of the model here that you want to use.
+method_name = "KNeighborsDist_BAG_L1" # Please enter the name of the model here that you want to use.
 
 model_paths = {
-    'dense': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-12-2022--02:15:12-pretrained-dense',
-    'conv': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-12-2022--02:02:38-pretrained-conv',
-    'pool': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-12-2022--02:25:59-pretrained-pool'
+    'dense': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:24:57-pretrained-dense',
+    'conv': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:26:12-pretrained-conv',
+    'pool': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:28:18-pretrained-pool',
+    'inputlayer': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:28:55-pretrained-inputlayer',
+    'pad': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:29:26-pretrained-pad',
+    'normalization': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:30:32-pretrained-normalization',
+    'activation': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:32:52-pretrained-activation',
+    'rescaling': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:34:31-pretrained-rescaling',
+    'reshape': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:35:01-pretrained-reshape',
+    'dropout': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:36:16-pretrained-dropout',
+    'add': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:37:26-pretrained-add',
+    'multiply': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:38:57-pretrained-multiply',
+    'concatenate': '/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/agModels-greenML-07-14-2022--18:40:03-pretrained-concatenate'
 } # TODO: Please add your models here manually.
 
 features = {
-    'dense': ["batch_size", "num_layer_total", "input_size", "output_size", "hidden_size"],
-    'conv': ["batch_size", "num_layer_total", "input_size", "output_size", "filters", "kernel_size", "stride"],
-    'pool': ["batch_size", "num_layer_total", "input_size", "output_size", "filters (default=1)", "pool_size", "stride"],
-    'inputlayer': ["batch_size", "num_layer_total", "input_size"],
-    'pad': ["batch_size", "num_layer_total", "input_size", "output_size", "padding"],
-    'normalization': ["batch_size", "num_layer_total", "input_size", "output_size"],
-    'activation': ["batch_size", "num_layer_total", "input_size"],
-    'rescaling': ["batch_size", "num_layer_total", "input_size"],
-    'reshape': ["batch_size", "num_layer_total", "input_size", "target_shape"],
-    'dropout': ["batch_size", "num_layer_total", "input_size", "rate"],
-    'add': ["batch_size", "num_layer_total", "output_size"],
-    'multiply': ["batch_size", "num_layer_total", "output_size"],
-    'concatenate': ["batch_size", "num_layer_total", "output_size"]
-}
+            'dense': ["batch_size", "num_layer_total", "input_size", "output_size", "hidden_size"],
+            'conv': ["batch_size", "num_layer_total", "input_size", "output_size", "filters", "kernel_size", "stride"],
+            'pool': ["batch_size", "num_layer_total", "input_size", "output_size", "filters (default=1)", "pool_size", "stride"],
+            'inputlayer': ["batch_size", "num_layer_total", "input_size"],
+            'pad': ["batch_size", "num_layer_total", "input_size", "output_size", "padding"],
+            'normalization': ["batch_size", "num_layer_total", "input_size", "output_size"],
+            'activation': ["batch_size", "num_layer_total", "input_size"],
+            'rescaling': ["batch_size", "num_layer_total", "input_size"],
+            'reshape': ["batch_size", "num_layer_total", "input_size", "target_shape"],
+            'dropout': ["batch_size", "num_layer_total", "input_size", "rate"],
+            'add': ["batch_size", "num_layer_total", "output_size"],
+            'multiply': ["batch_size", "num_layer_total", "output_size"],
+            'concatenate': ["batch_size", "num_layer_total", "output_size"]
+        }
 
-predictor_dense = TabularPredictor.load(model_paths['dense'])  # unnecessary, just demonstrates how to load previously-trained predictor from file
+predictor_dense = TabularPredictor.load(model_paths['dense'])
 method_index = predictor_dense.get_model_names().index(method_name)
 model_dense = predictor_dense.get_model_names()[method_index]
 
-predictor_conv = TabularPredictor.load(model_paths['conv'])  # unnecessary, just demonstrates how to load previously-trained predictor from file
-method_index = predictor_dense.get_model_names().index(method_name)
+predictor_conv = TabularPredictor.load(model_paths['conv'])
+method_index = predictor_conv.get_model_names().index(method_name)
 model_conv = predictor_conv.get_model_names()[method_index]
 
-predictor_pool = TabularPredictor.load(model_paths['pool'])  # unnecessary, just demonstrates how to load previously-trained predictor from file
-method_index = predictor_dense.get_model_names().index(method_name)
+predictor_pool = TabularPredictor.load(model_paths['pool'])
+method_index = predictor_pool.get_model_names().index(method_name)
 model_pool = predictor_pool.get_model_names()[method_index]
+
+predictor_inputlayer = TabularPredictor.load(model_paths['inputlayer'])
+method_index = predictor_inputlayer.get_model_names().index(method_name)
+model_inputlayer = predictor_inputlayer.get_model_names()[method_index]
+
+predictor_pad = TabularPredictor.load(model_paths['pad'])
+method_index = predictor_pad.get_model_names().index(method_name)
+model_pad = predictor_pad.get_model_names()[method_index]
+
+predictor_normalization = TabularPredictor.load(model_paths['normalization'])
+method_index = predictor_normalization.get_model_names().index(method_name)
+model_normalization = predictor_normalization.get_model_names()[method_index]
+
+predictor_activation = TabularPredictor.load(model_paths['activation'])
+method_index = predictor_activation.get_model_names().index(method_name)
+model_activation = predictor_activation.get_model_names()[method_index]
+
+predictor_rescaling = TabularPredictor.load(model_paths['rescaling'])
+method_index = predictor_rescaling.get_model_names().index(method_name)
+model_rescaling = predictor_rescaling.get_model_names()[method_index]
+
+predictor_reshape = TabularPredictor.load(model_paths['reshape'])
+method_index = predictor_reshape.get_model_names().index(method_name)
+model_reshape = predictor_reshape.get_model_names()[method_index]
+
+predictor_dropout = TabularPredictor.load(model_paths['dropout'])
+method_index = predictor_dropout.get_model_names().index(method_name)
+model_dropout = predictor_dropout.get_model_names()[method_index]
+
+predictor_add = TabularPredictor.load(model_paths['add'])
+method_index = predictor_add.get_model_names().index(method_name)
+model_add = predictor_add.get_model_names()[method_index]
+
+predictor_multiply = TabularPredictor.load(model_paths['multiply'])
+method_index = predictor_multiply.get_model_names().index(method_name)
+model_multiply = predictor_multiply.get_model_names()[method_index]
+
+predictor_concatenate = TabularPredictor.load(model_paths['concatenate'])
+method_index = predictor_concatenate.get_model_names().index(method_name)
+model_concatenate = predictor_concatenate.get_model_names()[method_index]
+
 
 dataset_class_name = config['dataset_class']
 dataset = LayerWiseDatasetv2Large(**config[dataset_class_name]['params'])
@@ -68,9 +119,41 @@ for i, model in enumerate(raw_x_test):
         elif layer_type == "pool":
             layerwise_prediction = predictor_pool.predict(df, model=model_pool).to_numpy().item()
             current_consumption += layerwise_prediction
+        elif layer_type == "inputlayer":
+            layerwise_prediction = predictor_inputlayer.predict(df, model=model_inputlayer).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "pad":
+            layerwise_prediction = predictor_pad.predict(df, model=model_pad).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "normalization":
+            layerwise_prediction = predictor_normalization.predict(df, model=model_normalization).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "activation":
+            layerwise_prediction = predictor_activation.predict(df, model=model_activation).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "rescaling":
+            layerwise_prediction = predictor_rescaling.predict(df, model=model_rescaling).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "reshape":
+            layerwise_prediction = predictor_reshape.predict(df, model=model_reshape).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "dropout":
+            layerwise_prediction = predictor_dropout.predict(df, model=model_dropout).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "add":
+            layerwise_prediction = predictor_add.predict(df, model=model_add).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "multiply":
+            layerwise_prediction = predictor_multiply.predict(df, model=model_multiply).to_numpy().item()
+            current_consumption += layerwise_prediction
+        elif layer_type == "concatenate":
+            layerwise_prediction = predictor_concatenate.predict(df, model=model_concatenate).to_numpy().item()
+            current_consumption += layerwise_prediction
         else:
             raise TypeError("Illegal layer type!")
-    y_modelwise_predicted.append(current_consumption*1e-9)
+    current_consumption = current_consumption * 1e-9
+    print(f"Predicted: {current_consumption}\tActual: {y_modelwise_true[i]}")
+    y_modelwise_predicted.append(current_consumption)
 
 print(len(y_modelwise_predicted))
 print(len(y_modelwise_true))
--- src/models/autogluon.py
+++ src/models/autogluon.py
@@ -4,49 +4,70 @@ from autogluon.core.metrics import make_scorer
 from metrics import rmspe
 import pandas as pd
 
-# TODO: The following parameters need to be set manually!
-# Begin Parameters
-dataset_class = 'LayerWiseDatasetv2Large' # Fixed.
-subset = 'pretrained' # Fixed.
-target_layer = 'conv'
-# End Parameters
+features = {
+    'dense': ["batch_size", "num_layer_total", "input_size", "output_size", "hidden_size"],
+    'conv': ["batch_size", "num_layer_total", "input_size", "output_size", "filters", "kernel_size", "stride"],
+    'pool': ["batch_size", "num_layer_total", "input_size", "output_size", "filters (default=1)", "pool_size",
+             "stride"],
+    'inputlayer': ["batch_size", "num_layer_total", "input_size"],
+    'pad': ["batch_size", "num_layer_total", "input_size", "output_size", "padding"],
+    'normalization': ["batch_size", "num_layer_total", "input_size", "output_size"],
+    'activation': ["batch_size", "num_layer_total", "input_size"],
+    'rescaling': ["batch_size", "num_layer_total", "input_size"],
+    'reshape': ["batch_size", "num_layer_total", "input_size", "target_shape"],
+    'dropout': ["batch_size", "num_layer_total", "input_size", "rate"],
+    'add': ["batch_size", "num_layer_total", "output_size"],
+    'multiply': ["batch_size", "num_layer_total", "output_size"],
+    'concatenate': ["batch_size", "num_layer_total", "output_size"]
+}
 
-now = datetime.now() # current date and time
-timestamp = now.strftime("%m-%d-%Y--%H:%M:%S")
+target_layers = list(features.keys())
 
-time_limit = 600 # TODO: Change the time limit for faster results.
-presets='best_quality' # Available presets: [‘best_quality’, ‘high_quality’, ‘good_quality’, ‘medium_quality’]. Please use 'best_quality' if possible.
+for target_layer in target_layers:
 
-train_data_path = f'/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/{dataset_class}-{subset}-{target_layer}-train.csv'
-test_data_path = f'/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/models/{dataset_class}-{subset}-{target_layer}-test.csv'
+    # TODO: The following parameters need to be set manually!
+    # Begin Parameters
+    dataset_class = 'LayerWiseDatasetv2Large' # Fixed.
+    subset = 'pretrained' # Fixed.
+    target_layer = target_layer
+    # End Parameters
 
-train_data = pd.read_csv(train_data_path, index_col=0)
-train_data = TabularDataset(train_data)
-print(train_data.head())
+    now = datetime.now() # current date and time
+    timestamp = now.strftime("%m-%d-%Y--%H:%M:%S")
 
-label = 'energy_consumption'
-print("Summary of class variable: \n", train_data[label].describe())
+    time_limit = 60 # TODO: Change the time limit for faster results.
+    presets='best_quality' # Available presets: [‘best_quality’, ‘high_quality’, ‘good_quality’, ‘medium_quality’]. Please use 'best_quality' if possible.
 
-save_path = f'agModels-greenML-{timestamp}-{subset}-{target_layer}'  # specifies folder to store trained models
-predictor = TabularPredictor(label=label, path=save_path).fit(train_data, time_limit=time_limit, presets=presets)
+    train_data_path = f'/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/{dataset_class}-{subset}-{target_layer}-train.csv'
+    test_data_path = f'/home/denizkorkmaz/PycharmProjects/TUM/SS22/green-ml-daml/src/{dataset_class}-{subset}-{target_layer}-test.csv'
 
-test_data = pd.read_csv(train_data_path, index_col=0)
-test_data = TabularDataset(test_data)
-y_test = test_data[label]  # values to predict
-test_data_nolab = test_data.drop(columns=[label])  # delete label column to prove we're not cheating
-test_data_nolab.head()
+    train_data = pd.read_csv(train_data_path, index_col=0)
+    train_data = TabularDataset(train_data)
+    print(train_data.head())
 
-y_pred = predictor.predict(test_data_nolab)
-print("Predictions:  \n", y_pred)
-perf = predictor.evaluate_predictions(y_true=y_test, y_pred=y_pred, auxiliary_metrics=True)
+    label = 'energy_consumption'
+    print("Summary of class variable: \n", train_data[label].describe())
 
-ag_rmspe_custom_scorer = make_scorer(
-    name='rmspe',
-    score_func=rmspe,
-    optimum=0,
-    greater_is_better=False)
+    save_path = f'agModels-greenML-{timestamp}-{subset}-{target_layer}'  # specifies folder to store trained models
+    predictor = TabularPredictor(label=label, path=save_path).fit(train_data, time_limit=time_limit, presets=presets)
 
-predictor.leaderboard(test_data, extra_metrics=[ag_rmspe_custom_scorer]).to_csv(f"{timestamp}-results-{dataset_class}-{subset}-{target_layer}.csv", encoding='utf-8', index=False)
+    test_data = pd.read_csv(train_data_path, index_col=0)
+    test_data = TabularDataset(test_data)
+    y_test = test_data[label]  # values to predict
+    test_data_nolab = test_data.drop(columns=[label])  # delete label column to prove we're not cheating
+    test_data_nolab.head()
 
-y_pred_np = y_pred.to_numpy().flatten()
-y_test_np = y_test.to_numpy().flatten()
+    y_pred = predictor.predict(test_data_nolab)
+    print("Predictions:  \n", y_pred)
+    perf = predictor.evaluate_predictions(y_true=y_test, y_pred=y_pred, auxiliary_metrics=True)
+
+    ag_rmspe_custom_scorer = make_scorer(
+        name='rmspe',
+        score_func=rmspe,
+        optimum=0,
+        greater_is_better=False)
+
+    predictor.leaderboard(test_data, extra_metrics=[ag_rmspe_custom_scorer]).to_csv(f"{timestamp}-results-{dataset_class}-{subset}-{target_layer}.csv", encoding='utf-8', index=False)
+
+    y_pred_np = y_pred.to_numpy().flatten()
+    y_test_np = y_test.to_numpy().flatten()
