62061230e201dedcd627c443faba4532ced019aa
barandenizkorkmaz <deniz.korkmaz@boun.edu.tr>
2022-07-29 00:54:56 +0200

    Add requirements.txt

--- src/dataset/experiments/test.py
+++
@@ -1,9 +0,0 @@
-import energyEvaluation as eval
-import tensorflow as tf
-
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(128, activation = "relu"),
-    tf.keras.layers.Dense(10)
-])
-
-print(eval.evaluate_energy_forward(model, input_size = (10,), batch_size = 10, repetitions = 10))
